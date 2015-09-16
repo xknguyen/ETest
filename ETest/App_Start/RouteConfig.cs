c536ba9339fb98c29b6c079acfd5b6b39d12dc39
xknguyen <nguyenxuankhoactk35@gmail.com>
2015-09-16 13:19:09 +0700

    update

--- ETest/App_Start/RouteConfig.cs
+++ ETest/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Xml.XPath;
 
 namespace ETest
 {
@@ -16,8 +17,9 @@ namespace ETest
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new {controller = "Home", action = "Index", id = UrlParameter.Optional},
+                namespaces: new[] {"ETest.Controllers"}
+                );
         }
     }
 }
