351bf61437c04373f64dd7acbc51f3faa8ef26c5
xknguyen <nguyenxuankhoactk35@gmail.com>
2015-09-16 09:35:24 +0700

    Create project

---
+++ ETest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ETest.Startup))]
+namespace ETest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
