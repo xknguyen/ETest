c44679c78523c07e3257bf4ad9cec8a001061cab
xknguyen <nguyenxuankhoactk35@gmail.com>
2015-11-05 20:10:15 +0700

    ads

---
+++ ETest/Scripts/model/TestModel.js
@@ -0,0 +1,26 @@
+﻿function TestModel(id, title, description, course, timeStart, timeEnd, testTime, mixedQuestion, submitNo, gradeType, actived, details) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.course = course;
+    this.timeStart = timeStart;
+    this.timeEnd = timeEnd;
+    this.testTime = testTime;
+    this.mixedQuestion = mixedQuestion;
+    this.submitNo = submitNo;
+    this.gradeType = gradeType;
+    this.actived = actived;
+    this.details = details;
+}
+
+function TestDetailModel(id, score, order, details) {
+    this.id = id;
+    this.score = score;
+    this.order = order;
+    this.details = details;
+}
+
+function QuestionDetail(id, score) {
+    this.id = id;
+    this.score = score;
+}
